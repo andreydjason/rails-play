4a8543fb4bd1e96fd0969fd8ed2ad78a131ebc09
Andrey Djason Viana <andreydjason@gmail.com>
2012-09-24 05:36:17 -0300

    updts

--- app/assets/javascripts/init-0.js
+++ app/assets/javascripts/init-0.js
@@ -1,5 +1,9 @@
-dojo.ready(function() {
-  
-  console.info('Initiating...');
-  
-})
+require(["dojo/dom", "dojo/router", "dojo/domReady!"], function(router, dom) {
+  var greeting;
+
+  router.register("/articles/:id", function(evt) {
+    return alert("The article has the id " + evt[id]);
+  });
+
+  router.startup();
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
 
 	#------------------------------
-  def current_full_domain_url
-    "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+  def full_current_domain_url
+    "#{request.protocol}#{request.host_with_port}"
+  end
+
+  #------------------------------
+  def full_current_domain_url_with_fullpath
+    "#{full_current_domain_url}#{request.fullpath}"
   end
 
 end
